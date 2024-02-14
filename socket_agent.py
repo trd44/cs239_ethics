7fa2e3eebb57ffa935de15506c671aec9096fe0a
Matthias Scheutz <matthias.scheutz@tufts.edu>
2024-02-14 08:36:08 -0500

    MS: printing observations and violations

--- socket_agent.py
+++ socket_agent.py
@@ -38,4 +38,5 @@ if __name__ == "__main__":
         output = recv_socket_data(sock_game)  # get observation from env
         output = json.loads(output)
 
-        print("JSON: ", output)
+        print("Observations: ", output["observation"])
+        print("Violations", output["violations"])
